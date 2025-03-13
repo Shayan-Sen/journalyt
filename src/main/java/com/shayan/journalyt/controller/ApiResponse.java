4f9cdc9d85c348cb7ab487ccfd63fc1ae85b06cd
Shayan Sen <senshayan2103@gmail.com>
2025-03-13 14:21:03 +0530

    felt cute so added annotation

--- src/main/java/com/shayan/journalyt/controller/ApiResponse.java
+++ src/main/java/com/shayan/journalyt/controller/ApiResponse.java
@@ -4,11 +4,13 @@ import org.springframework.hateoas.RepresentationModel;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse extends RepresentationModel<ApiResponse> {
     private Object data;
     private String message;
