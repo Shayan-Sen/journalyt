208a12b186811968f7f7ad04f149a8d3de3abcfa
Shayan Sen <senshayan2103@gmail.com>
2025-03-23 02:25:52 +0530

    added  junit-test

---
+++ src/test/java/com/shayan/journalyt/service/UserServiceTest.java
@@ -0,0 +1,21 @@
+package com.shayan.journalyt.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.shayan.journalyt.repository.UserRepository;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUserName(){
+        assertNotNull(userRepository.findByUsername("writer1"));
+    }
+}
