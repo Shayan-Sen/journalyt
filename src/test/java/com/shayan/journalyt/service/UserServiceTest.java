552bd045da95c21f3c77759f04ae14b4a6ebbb93
Shayan Sen <senshayan2103@gmail.com>
2025-05-10 13:09:38 +0530

    test: update testFindByUserName to use admin username
    - Changed test username from "shayan" to "admin" in UserServiceTest
    - Ensures test uses the default admin user for consistency

--- src/test/java/com/shayan/journalyt/service/UserServiceTest.java
+++ src/test/java/com/shayan/journalyt/service/UserServiceTest.java
@@ -16,6 +16,6 @@ public class UserServiceTest {
 
     @Test
     public void testFindByUserName(){
-        assertNotNull(userRepository.findByUsername("shayan"));
+        assertNotNull(userRepository.findByUsername("admin"));
     }
 }
